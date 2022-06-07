894b2641817250ad78d65183a1b536efbdf3610c
Salman <salman@salman.beeline>
2022-06-07 18:26:57 +0300

    feat: Create user entity and userModel

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { InjectModel } from 'nestjs-typegoose'
+import { UserModel } from '../user/user.model'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>) {}
+
+  async register(dto: any) {
+    return this.
+  }
+}

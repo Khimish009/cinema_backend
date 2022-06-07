48388e3fc89eab7e6be40053689618f2a7786458
Salman <salman@salman.beeline>
2022-06-07 18:33:39 +0300

    feat: Add TimeStamps and update userModel

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,6 +1,18 @@
-export class UserModel {
+import { prop } from '@typegoose/typegoose'
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+
+export interface UserModel extends Base {}
+
+export class UserModel extends TimeStamps {
+  @prop({ unique: true })
   email: string
+
+  @prop()
   password: string
+
+  @prop({ defauslt: false })
   isAdmin: boolean
+
+  @prop({ default: [] })
   favorites: []
 }
